be2ca27b6de837fd7900d68f3416e1fad4f22742
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:06:47 -0300

    Add files via upload

---
+++ 53-BuscadorDePersonas02/BuscadorDePersonas03/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace BuscadorDePersonas03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ListadoDeNombres = new string[5];
+
+            Console.WriteLine("Ingrese 5 nombres para guardar en el listado:");
+
+            for (int i=0; i < ListadoDeNombres.Length; i++)
+            {
+                ListadoDeNombres[i] = Console.ReadLine();
+                Console.Clear();
+            }
+
+            Console.WriteLine("Los nombres han sido ingresados, se les han asignado un numero desde el 0 hasta el 4 de acuerdo a su orden de entrada");
+            Console.WriteLine("Ingrese un numero para que se muestre el nombre que busca o ingrese -1 para cerrar:");
+
+            int NumeroDeNombre = Convert.ToInt32(Console.ReadLine());
+
+            
+                while (NumeroDeNombre != -1)
+                {
+                    for (int i = 0; i < ListadoDeNombres.Length; i++)
+                    {
+                        if (i == NumeroDeNombre)
+                        {
+                            Console.WriteLine("El numero ingresado da como resultado: " + ListadoDeNombres[i]);
+                        }
+                    }
+                    Console.WriteLine("Ingrese otro numero para ver otro nombre de la lista:");
+                    NumeroDeNombre = Convert.ToInt32(Console.ReadLine());
+
+                }
+            
+
+            //while (NumeroDeNombre != -1)
+            //{
+            //    for (int i = 0; i < ListadoDeNombres.Length; i++)
+            //    {
+            //        if (i == NumeroDeNombre)
+            //        {
+            //            Console.WriteLine("El numero ingresado da como resultado: " + ListadoDeNombres[i]);
+            //        }
+            //    }
+            //    Console.WriteLine("Ingrese otro numero para ver otro nombre de la lista:");
+            //    NumeroDeNombre = Convert.ToInt32(Console.ReadLine());
+
+            //}
+            //for (int i=0; i<ListadoDeNombres.Length; i++)
+            //{
+            //    if (i== NumeroDeNombre)
+            //    {
+            //        Console.WriteLine("El numero ingresado da como resultado: " + ListadoDeNombres[i]);
+            //    }
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
